f3146ae54c6983fffc3c536932c0da4879bc1816
euglazer <eugenestapler@gmail.com>
2015-03-19 08:03:43 +1300

    remove driver test code

--- bacteria.rb
+++ bacteria.rb
@@ -8,15 +8,3 @@ class Bacteria
   end
 
 end
-
-bacteria = Bacteria.new
-puts "bacteria is by default alive"
-puts bacteria.alive == true
-puts "bacteria has zero neighbors"
-puts bacteria.neighbors == 0
-
-bacteria = Bacteria.new(1)
-puts "bacteria with probability 1 is not alive"
-puts bacteria.alive == false
-puts "..and still has 0 neighbors"
-puts bacteria.neighbors == 0
