6ba83204a08dc8ec78539722805ac39d21f6860a
euglazer <eugenestapler@gmail.com>
2015-03-19 08:02:57 +1300

    add bacteria tests

---
+++ bacteria_spec.rb
@@ -0,0 +1,28 @@
+require_relative "./bacteria.rb"
+
+describe "Bacteria" do
+
+  describe "#initialize(probability)" do
+
+    it "assigns neighbors to 0" do
+      expect(Bacteria.new.neighbors).to eq(0)
+    end
+
+    it "if probability unspecified, assigns alive to true" do
+      bacteria = Bacteria.new
+      expect(bacteria.alive).to be_truthy
+    end
+
+    it "if probability is 0, assigns alive to true" do
+      bacteria = Bacteria.new(0)
+      expect(bacteria.alive).to be_truthy
+    end
+
+    it "if probability is 1, assigns alive to false" do
+      bacteria = Bacteria.new(1)
+      expect(bacteria.alive).to be_falsey
+    end
+
+  end
+
+end
